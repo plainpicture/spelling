644666d5656bea439ab7a273123144df63253b37
Benjamin Vetter <vetter@plainpicture.de>
2016-03-03 10:09:15 +0100

    start.sh changed

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-/bin/ls data/*.dic | /usr/bin/sort | /usr/bin/xargs /usr/bin/java -jar target/spelling-0.0.1-jar-with-dependencies.jar
+exec /usr/bin/java -jar target/spelling-0.0.1-jar-with-dependencies.jar `ls data/*.dic | sort`
 
