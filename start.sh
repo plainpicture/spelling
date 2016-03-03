4d52dbe2c6f7fb1f9eeea30681aee0f7e14a1c40
Benjamin Vetter <vetter@plainpicture.de>
2016-03-03 11:08:03 +0100

    Setting memory constraints in start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-exec /usr/bin/java -jar target/spelling-0.0.1-jar-with-dependencies.jar `ls data/*.dic | sort`
+exec /usr/bin/java -Xms512m -Xmx1024m -jar target/spelling-0.0.1-jar-with-dependencies.jar `ls data/*.dic | sort`
 
