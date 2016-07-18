1c3f9104a47b7cf02874bda15b41a3ecb3c9d58d
Benjamin Vetter <vetter@plainpicture.de>
2016-07-18 14:30:44 +0200

    Using exec in start.sh again

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-/usr/bin/java ${JAVA_OPTS} -jar target/spelling-0.0.1-jar-with-dependencies.jar `ls data/*.dic | sort`
+exec /usr/bin/java ${JAVA_OPTS} -jar target/spelling-0.0.1-jar-with-dependencies.jar `ls data/*.dic | sort`
 
